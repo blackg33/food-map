8dfe6f410c71bc112cc629a05595a13a5c1563b2
[Gillian Black] <[gillian@zulualphakilo.com]>
2018-09-30 12:26:31 -0400

    initial commit

---
+++ public/js/main.js
@@ -0,0 +1,78 @@
+var map;
+        
+/*GOOGLE MAPS JS API*/
+/********************/
+/*WIZARD TO PLAY WITH MAP OPTIONS/STYLES AND GENERATE CODE: https://mapstyle.withgoogle.com/*/
+
+function initMap(){
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 43.6532, lng:-79.3832},
+        zoom:13,
+        /*DISABLE DEFAULT UI CONTROLS*/
+//                disableDefaultUI: true,
+        mapTypeControl: false,
+         styles: [
+    {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+    {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+    {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+    {
+      featureType: 'administrative.locality',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#d59563'}]
+    },
+               {
+    featureType: "poi",
+    stylers: [{visibility: "off"}]
+  },
+    {
+      featureType: 'road',
+      elementType: 'geometry',
+      stylers: [{color: '#38414e'}]
+    },
+    {
+      featureType: 'road',
+      elementType: 'geometry.stroke',
+      stylers: [{color: '#212a37'}]
+    },
+    {
+      featureType: 'road',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#9ca5b3'}]
+    },
+    {
+      featureType: 'road.highway',
+      elementType: 'geometry',
+      stylers: [{color: '#746855'}]
+    },
+    {
+      featureType: 'road.highway',
+      elementType: 'geometry.stroke',
+      stylers: [{color: '#1f2835'}]
+    },
+    {
+      featureType: 'road.highway',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#f3d19c'}]
+    },
+   {
+    featureType: "transit",
+    stylers: [{visibility: "off"}]
+   },
+    {
+      featureType: 'water',
+      elementType: 'geometry',
+      stylers: [{color: '#17263c'}]
+    },
+    {
+      featureType: 'water',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#515c6d'}]
+    },
+    {
+      featureType: 'water',
+      elementType: 'labels.text.stroke',
+      stylers: [{color: '#17263c'}]
+    }
+  ]
+    });
+}
